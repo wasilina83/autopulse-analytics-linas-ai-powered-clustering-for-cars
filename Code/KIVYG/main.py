6a9b5fa5471310f5a4d9b7455ead86678a1c97cb
wasilina83 <angelinaengelmann@live.de>
2024-04-06 00:18:53 +0200

    h

--- Code/KIVYG/main.py
+++ Code/KIVYG/main.py
@@ -111,7 +111,7 @@ class SignalclassifierApp(App):
         self.layout.add_widget(self.prolab)
 
         # Add other widgets or elements if needed
-        self.text_label3 = Label(text='Signalgenerator starten', font_size=win_h*.1, pos_hint={'center_x': .1, 'top':  1})
+        self.text_label3 = Label(text=f'Signalgenerator \n   starten', font_size=win_h*.05, pos_hint={'center_x': .2, 'top':  1})
         self.layout.add_widget(self.text_label3)
 
         # Load the image for the Play button
