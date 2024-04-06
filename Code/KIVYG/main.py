90ea21d37fecbf21564cb17b5e9c01770ca48db4
wasilina83 <angelinaengelmann@live.de>
2024-04-06 05:40:01 +0200

    b2

--- Code/KIVYG/main.py
+++ Code/KIVYG/main.py
@@ -210,7 +210,7 @@ class SignalclassifierApp(App):
         # Add a Combobox for signal type
         signal_types = ['Batterie', 'Lichtmaschine']
         
-        self.signal_type_spinner = Spinner(text='Lichtmaschine', values=signal_types, size_hint=(None, None), size=(int(window_width*.3), int(window_width*.03)), pos_hint={'center_x': 0.2, 'top': 0.55}, background_color=utils.get_color_from_hex('#0046F0'), outline_color=(0, 0.2745, 0.9412, 1), disabled_outline_color=(0, 0.2745, 0.9412, 1), color='white', font_context='system://myapp', font_name='OpenSans-Bold.ttf')
+        self.signal_type_spinner = Spinner(text='Lichtmaschine', values=signal_types, size_hint=(None, None), size=(int(window_width*.2), int(window_width*.03)), pos_hint={'center_x': 0.2, 'top': 0.55}, background_color=utils.get_color_from_hex('#0046F0'), outline_color=(0, 0.2745, 0.9412, 1), disabled_outline_color=(0, 0.2745, 0.9412, 1), color='white', font_context='system://myapp', font_name='OpenSans-Bold.ttf')
         self.layout.add_widget(self.signal_type_spinner)
 
         # Load the Play button
